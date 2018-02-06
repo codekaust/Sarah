3bef178221adbadcd49ba7bcdebd2871c38a0fe6
codekaust <kaustubh1303@gmail.com>
2018-02-07 01:53:28 +0530

    added geofence triggers with basic notifs

--- app/src/main/java/com/triointeli/sarah/GeofenceTransitionService.java
+++ app/src/main/java/com/triointeli/sarah/GeofenceTransitionService.java
@@ -7,7 +7,9 @@ import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
 import android.text.TextUtils;
@@ -17,10 +19,12 @@ import android.widget.Toast;
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofenceStatusCodes;
 import com.google.android.gms.location.GeofencingEvent;
+import com.triointeli.sarah.DatabaseModels.Reminder;
 import com.triointeli.sarah.DatabaseModels.YourPlaces;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import io.realm.Realm;
 import io.realm.RealmResults;
@@ -43,34 +47,84 @@ public class GeofenceTransitionService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        realm=Realm.getDefaultInstance();
+        realm = Realm.getDefaultInstance();
+
+        Random rn = new Random();
 
         GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
 
         // Handling errors
-        if ( geofencingEvent.hasError() ) {
-            String errorMsg = getErrorString(geofencingEvent.getErrorCode() );
-            Log.e( TAG, errorMsg );
+        if (geofencingEvent.hasError()) {
+            String errorMsg = getErrorString(geofencingEvent.getErrorCode());
+            Log.e(TAG, errorMsg);
             return;
         }
 
         int geoFenceTransition = geofencingEvent.getGeofenceTransition();
 
         // Check if the transition type is of interest
-        if ( geoFenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+        if (geoFenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
 
             // Get the geofence that were triggered
             List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
 
+            RealmResults<Reminder> realmReminderResult = realm.where(Reminder.class).equalTo("placeOnEnter", triggeringGeofences.get(0).getRequestId()).findAll();
+
+            // Use an iterator to give all notifs
+            realm.beginTransaction();
+
+            for (Reminder rnNDR : realmReminderResult) {
+                Intent intentNotify = new Intent();
+                PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+                builder.setSmallIcon(R.drawable.logo_sarah);
+                builder.setContentIntent(pendingIntent);
+                builder.setAutoCancel(true);
+                builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.logo_sarah));
+                builder.setContentTitle("Enterred" + rnNDR.getPlaceOnEnter());
+                builder.setVibrate(new long[]{1000, 1000, 1000, 1000, 1000});
+                builder.setContentText(rnNDR.getReminderContent());
+                NotificationManager notificationManager = (NotificationManager) getSystemService(
+                        NOTIFICATION_SERVICE);
+                notificationManager.notify(rn.nextInt(50), builder.build());
+
+            }
+
+            realm.commitTransaction();
             //String geofenceTransitionDetails = getGeofenceTransitionDetails(geoFenceTransition, triggeringGeofences );
 
             // Send notification details as a String
         }
 
-        if(geoFenceTransition==Geofence.GEOFENCE_TRANSITION_EXIT){
+        if (geoFenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
 
+
+            // Get the geofence that were triggered
             List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
 
+            RealmResults<Reminder> realmReminderResult = realm.where(Reminder.class).equalTo("placeOnLeave", triggeringGeofences.get(0).getRequestId()).findAll();
+
+            // Use an iterator to give all notifs
+            realm.beginTransaction();
+
+            for (Reminder rnNDR : realmReminderResult) {
+                Intent intentNotify = new Intent();
+                PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+                builder.setSmallIcon(R.drawable.logo_sarah);
+                builder.setContentIntent(pendingIntent);
+                builder.setAutoCancel(true);
+                builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.logo_sarah));
+                builder.setContentTitle("Left" + rnNDR.getPlaceOnLeave());
+                builder.setVibrate(new long[]{1000, 1000, 1000, 1000, 1000});
+                builder.setContentText(rnNDR.getReminderContent());
+                NotificationManager notificationManager = (NotificationManager) getSystemService(
+                        NOTIFICATION_SERVICE);
+                notificationManager.notify(rn.nextInt(50), builder.build());
+
+            }
         }
     }
 
